ed5ae161449a5509befc4abb0f3742becca93e95
highgon2 <highgon2@gmail.com>
2019-02-06 10:50:07 +0900

    messagebox font size

--- Tkinter/15_messagebox.py
+++ Tkinter/15_messagebox.py
@@ -4,6 +4,7 @@ import tkinter.messagebox		# Messagebox를 그릴 경우 반드시 import 해야
 windows = tkinter.Tk()
 windows.title("Messagebox Test")
 windows.geometry("480x320+100+100")
+windows.option_add("*Dialog.msg.font", "맑은고딕 10")
 windows.resizable(0, 0)
 
 def draw_popup():
