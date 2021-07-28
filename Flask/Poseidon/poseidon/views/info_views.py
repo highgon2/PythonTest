d536293ed20f27feeec6e1b9c5d688188160c22a
gon <sglee@handan.co.kr>
2021-07-28 10:11:39 +0900

    Flask 수집 자료 페이징 기능 추가

--- Flask/Poseidon/poseidon/views/info_views.py
+++ Flask/Poseidon/poseidon/views/info_views.py
@@ -1,9 +1,11 @@
-from flask import Blueprint, render_template, url_for
+from flask import Blueprint, render_template, request, url_for
 from ..models import DeviceInfo
 
 bp = Blueprint('info', __name__, url_prefix='/info')
 
 @bp.route('/list/')
 def lists():
+    page = request.args.get('page', type=int, default=1)
     info_list = DeviceInfo.query.order_by(DeviceInfo.index.desc())
+    info_list = info_list.paginate(page, per_page=10)
     return render_template('info/devinfo_list.html', info_list=info_list)
